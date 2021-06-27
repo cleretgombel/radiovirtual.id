76e0fdbbc143a8ff96fcc553be5a20450601010e
cleretgombel <76208420+cleretgombel@users.noreply.github.com>
2021-06-27 22:29:25 +0700

    Create SyaratDanKetentuan.php

---
+++ .github/workflows/blank.yml/SyaratDanKetentuan.php
@@ -0,0 +1,14 @@
+SYARAT DAN KETENTUAN PENGGUNAAN APLIKASI RADIO VIRTUAL INDONESIA
+
+Aplikasi RVI hanya digunakan untuk layanan  berbasis komunikasi dua arah yang positif tidak buat komunikasi sara dan politik.
+Aplikasi RVI tidak boleh disalahgunakan untuk menipu, merugikan, dan/atau menimbulkan ketidaknyamanan kepada pihak lain.
+Pengguna RVI tidak diperbolehkan bicarakan server dan aplikasi lain yang bertujuan mengadu domba.
+RVI tidak bertanggung jawab atas kesalahan pengunduhan versi aplikasi yang dilakukan pengguna dan ketidakcocokan perangkat yang digunakan pengguna, serta hal-hal yang terjadi akibatnya.
+Jika pengguna terbukti menggunakan aplikasi untuk tujuan yang membahayakan, merugikan, atau di luar tujuan penggunaan yang dimaksud aplikasi ini, maka Pengurus RVI berhak melarang pengguna untuk menggunakan aplikasi RVI.
+RVI juga berhak melarang pengguna yang terbukti menggunakan aplikasi dengan perangkat yang tidak cocok atau tidak sah.
+Pengguna wajib menjaga kerukunan modulasi dengan teman yang didapatkan di aplikasi RVI dan tidak menyalahgunakannya untuk tujuan yang merugikan pihak lain.
+Pengguna dihimbau untuk tidak melakukan segala bentuk tindakan yang merugikan atau menimbulkan ketidaknyamanan, seperti mengancam atau melecehkan, saat menggunakan aplikasi RVI
+Pengguna dilarang menggunakan informasi/ program/ fitur yang terdapat di RVI  untuk melanggar peraturan perundang-undangan yang berlaku di Indonesia.
+Pengguna bertanggung jawab penuh atas akun RVI miliknya dan dapat dimintai pertanggungjawaban jika sewaktu-waktu terjadi hal yang negatif yang disebabkan oleh akunnya, meskipun akun tersebut disalahgunakan oleh pihak lain
+Jika perangkat pengguna diretas atau dicuri sehingga pengguna kehilangan kontrol atas akunnya, maka pengguna wajib memberitahu  sesegera mungkin agar kami dapat memblokir akun pengguna dan melakukan tindak pencegahan lainnya.
+🙏TERIMA KASIH 🙏
